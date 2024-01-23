4a60dc89856e688d16c75a00b21810a7d6e615d6
Yusuke710 <youfu1202mo@gmail.com>
2024-01-23 11:00:35 +1100

    open in different port

--- environment/frontend_server/manage.py
+++ environment/frontend_server/manage.py
@@ -1,3 +1,4 @@
+'''
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
@@ -16,6 +17,36 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+
+if __name__ == '__main__':
+    main()
+'''
+
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'frontend_server.settings')
+    
+    # Check if a port number is provided as a command-line argument
+    port = 9000  # Default port if not provided
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
+        port = int(sys.argv[1])
+    
+    # Update the command-line arguments to include the specified port
+    sys.argv = [sys.argv[0], 'runserver', f'localhost:{port}']
+    
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
